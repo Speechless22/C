e3558bb75b84097db612102514b060fa675cf3d1
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2022-09-15 20:07:34 +0800

    存

--- sxhs.c
+++ sxhs.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 int main()
 {
-    int hun, ten, ind, n;
+    int a, b, c, n;
     printf("result is:");
     for( n=100; n<1000; n++ )  /*整数的取值范围*/
     {
-        hun = n / 100;
-        ten = (n-hun*100) / 10;
-        ind = n % 10;
-        if(n == hun*hun*hun + ten*ten*ten + ind*ind*ind)  /*各位上的立方和是否与原数n相等*/
+        a = n / 100;
+        b = (n-a*100) / 10;
+        c = n % 10;
+        if(n == a*a*a + b*b*b + c*c*c)  /*各位上的立方和是否与原数n相等*/
             printf("%d  ", n);
     }
     printf("\n");
