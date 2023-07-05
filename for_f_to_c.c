b630d69d15a7a572cec393a05a54e4e4928ee4c4
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-07-05 09:29:29 +0800

    define

--- for_f_to_c.c
+++ for_f_to_c.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 //当fahr=0,20,40,...,300时，分别打印华氏温度与摄氏温度对照表
 main()
 {
     float fahr;
-    for (fahr =300; fahr >= 0; fahr = fahr - 20)
+    for (fahr =UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%6.1f\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
