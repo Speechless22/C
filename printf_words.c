254c54e62a0591ba929fc41c76aca9930cc4e6e4
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-07-06 15:02:38 +0800

    c

---
+++ printf_words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main ()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        {
+            putchar(c);
+        }
+        else
+        {
+            putchar('\n');
+        }
+    }
+}
